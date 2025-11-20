3c51e3cec5c57335b77f1528ed8487876fee7ef7
jerome j <jeromesiman388@example.com>
2025-11-20 11:30:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import cv2
+try:
+    # img=cv2.imread(r"C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\testfriut\test\download (2).jpg")
+    
+    try:
+        model=YOLO(r'C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\yolo11m.pt')
+        model.predict(r'C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\testfriut', save=True, show_conf = True, show_labels=True)
+    except Exception as e:
+        print(f"error on model:{e}")
+
+except Exception as e:
+    print(f"error on image: {e}")
