ab48527f08e84149a8accec34dc2c7f4c899f16f
jerome j <jeromesiman388@example.com>
2025-11-20 12:19:30 +0530

    added cv img input

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 import cv2
 try:
-    # img=cv2.imread(r"C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\testfriut\test\download (2).jpg")
+    img=cv2.imread(r"C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\testfriut\test\download (2).jpg")
     
     try:
         model=YOLO(r'C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\yolo11m.pt')
-        model.predict(r'C:\Users\jerom\OneDrive\Desktop\pratices\API\assessment\dl\testfriut', save=True, show_conf = True, show_labels=True)
+        model.predict(img, save=True, show_conf = True, show_labels=True)
     except Exception as e:
         print(f"error on model:{e}")
 
